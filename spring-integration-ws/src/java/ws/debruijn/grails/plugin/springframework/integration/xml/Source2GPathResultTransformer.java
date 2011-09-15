36bd2d019f069b3bf340cf93b0289cfe7b2be33b
Steef de Bruijn <steef@debruijn.ws>
2011-09-15 17:04:15 +0200

    Incorrect path for Source2GPathResultTransformer

--- spring-integration-ws/src/java/ws/debruijn/grails/plugin/springframework/integration/xml/Source2GPathResultTransformer.java
+++ spring-integration-ws/src/java/ws/debruijn/grails/plugin/springframework/integration/xml/Source2GPathResultTransformer.java
@@ -9,9 +9,9 @@ import javax.xml.transform.stream.StreamResult;
 import java.io.StringWriter;
 
 public class Source2GPathResultTransformer {
-    public GPathResult transform(Source message) throws Exception {
-        StringWriter sw = new StringWriter();
-        TransformerFactory.newInstance().newTransformer().transform(message, new StreamResult(sw));
-        return new XmlSlurper().parseText(sw.toString());
-    }
+  public GPathResult transform(Source message) throws Exception {
+    StringWriter sw = new StringWriter();
+    TransformerFactory.newInstance().newTransformer().transform(message, new StreamResult(sw));
+    return new XmlSlurper().parseText(sw.toString());
+  }
 }
